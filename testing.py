4b24f42671eed9ae141aa9a6b81b3ff89cb59906
MiguelMappleDev <mfelipevargassfa@gmail.com>
2024-06-15 02:05:51 -0400

    s

--- testing.py
+++ testing.py
@@ -6,16 +6,25 @@ import importlib.util
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-# %matplotlib inline
+# %matplotlib inline #only for jupyter
 from IPython.display import Image
-libraries = ["cv2", "numpy", "matplotlib.pyplot", "IPython"]
-
+from PIL import Image
+libraries = ["cv2", "numpy", "matplotlib.pyplot", "IPython", "PIL"]
 from validatelibraries import validatelibraries
 
 def main():
  
     validatelibraries(libraries)
 
+    # Open an image file
+    img = Image.open('src\checkerboard_18x18.png')
+    img2 = Image.open('src\checkerboard_84x84.jpg')
+    # Display image
+    img.show()
+    img2.show()
+    # Close image (optional)
+    img.close()
+    img2.close()
 
 
 if __name__ == "__main__":
